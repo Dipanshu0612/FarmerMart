a7d82354e2784e6add3dd05ed6eebcb5e7ad8ee3
Dipanshu Mishra <dipanshu.a.mishra06@gmail.com>
2025-02-25 08:07:47 +0000

    check order status route

--- app/api/seller/(product)/update-product/route.ts
+++ app/api/seller/(product)/update-product/route.ts
@@ -1,19 +1,21 @@
 import Order from "@/lib/models/ordersModel";
+import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   try {
+    await connectToDB();
     const { userId } = await auth();
     if (!userId) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
     const { order_id, order_status } = await req.json();
-    //   console.log(order_id, order_status);
+    console.log(order_id, order_status);
     await Order.findByIdAndUpdate(order_id, { order_status });
     return NextResponse.json(
-      { message: "Product updated successfully!", success:true },
+      { message: "Product updated successfully!", success: true },
       { status: 200 }
     );
   } catch (error) {
